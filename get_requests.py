b269f080146caa777e2885ca8659ddacdae5c185
Arsenij <motronyukov@bk.ru>
2024-05-18 16:34:01 +0500

    18.05.2024

--- get_requests.py
+++ get_requests.py
@@ -1,23 +1,23 @@
-import requests
+#import requests
 
 web =  'https://.com'
-result = requests.get(web)
-print(result)
-print('Код ответа от сервера:', result.status_code)
+#result = requests.get(web)
+#print(result)
+#print('Код ответа от сервера:', result.status_code)
 
 #print("Текст ответа!")
 #print(result.text)
 
 main_url = f'{web}ru/feed/'
 
-result = requests.get(web)
-data = result.json()
-print(data)
-url_for_task = requests.get('https//vk.com')
-print(url_for_task.ststus_code)
-main_url_2 = f'{web}?padge_i=5/'
-result = requests.get(main_url)
-print(result.status_code)
-result = requests.past(main_url_2, data = {'name': 'Masia, 'breed': 'siam','age': '20'})
-print(result.status_code)
-print(f'{web}?padge_i=5/')
+#result = requests.get(web)
+#data = result.json()
+#print(data)
+#url_for_task = requests.get('https//vk.com')
+#print(url_for_task.ststus_code)
+#main_url_2 = f'{web}?padge_i=5/'
+#result = requests.get(main_url)
+#print(result.status_code)
+#result = requests.past(main_url_2, data = {'name': 'Masia, 'breed': 'siam','age': '20'})
+#print(result.status_code)
+#print(f'{web}?padge_i=5/')
